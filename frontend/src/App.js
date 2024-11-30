cbcd744f58e46d97c6273f01037f860d365866a2
wfparrish <williparrish@gmail.com>
2024-11-30 14:44:16 -0700

    Updated the app and created video #3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Test from './Test';
 function App() {
   return (
     <div className="App">
-      <h1>RHCSA Practice App</h1>
       <Test />
     </div>
   );
--- frontend/src/Question.js
+++ frontend/src/Question.js
@@ -1,12 +1,18 @@
+// Question.js
 import React from 'react';
 import Step from './Step';
 
-const Question = ({ question, onNext, onPrevious, isFirst, isLast }) => {
+const Question = ({ question, onNext, onPrevious, isFirst, isLast, resetTrigger }) => {
   return (
     <div>
       <h2>{question.title}</h2>
       {question.steps.map((step) => (
-        <Step key={step.id} step={step} questionId={question.id} /> // Pass questionId here
+        <Step
+          key={step.id}
+          step={step}
+          questionId={question.id}
+          resetTrigger={resetTrigger} // Pass resetTrigger to Step
+        />
       ))}
       <div>
         {!isFirst && <button onClick={onPrevious}>Previous</button>}
